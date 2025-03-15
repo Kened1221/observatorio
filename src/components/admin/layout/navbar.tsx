5b5e5685a5aa934a0bbb6c592ae759e9035bf0ff
Miguel-UNSCH <miguelramirezgonzales48@gmail.com>
2025-03-14 21:40:45 -0500

    init admin

---
+++ src/components/admin/layout/navbar.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+import { ModeToggle } from "@/components/theme/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Bell,
+  Fullscreen,
+  Search,
+  Settings,
+} from "lucide-react";
+
+function Navbar() {
+
+  const handleToggleFullscreen = () => {
+    // Si no estamos en fullscreen, lo activamos
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.error("Error al intentar poner en fullscreen:", err);
+      });
+    } else {
+      // Si ya estamos en fullscreen, lo desactivamos
+      document.exitFullscreen().catch((err) => {
+        console.error("Error al intentar salir de fullscreen:", err);
+      });
+    }
+  };
+
+  return (
+    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-card/40 p-6 rounded-t-lg backdrop-blur-sm">
+      <SidebarTrigger className="hover:cursor-pointer" />
+      <div className="relative w-full max-w-sm">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Buscar..."
+          className="w-full bg-muted pl-8 focus-visible:ring-primary"
+        />
+      </div>
+      <div className="ml-auto flex items-center gap-4 text-muted-foreground">
+        <ModeToggle />
+        <Button variant="ghost" size="icon" className="hover:cursor-pointer" onClick={handleToggleFullscreen}>
+          <Fullscreen className="h-5 w-5" />
+          <span className="sr-only">Fullscreen</span>
+        </Button>
+        <Button variant="ghost" size="icon" className="relative hover:cursor-pointer">
+          <span className="absolute -right-0.5 -top-0.5 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-[10px] font-medium text-white">
+            3
+          </span>
+          <Bell className="h-5 w-5" />
+          <span className="sr-only">Notificaciones</span>
+        </Button>
+        <Button variant="ghost" size="icon" className="hover:cursor-pointer">
+          <Settings className="h-5 w-5" />
+          <span className="sr-only">Configuraciones</span>
+        </Button>
+        <div className="flex items-center gap-2">
+          <img
+            src="https://icons.veryicon.com/png/o/miscellaneous/user-avatar/user-avatar-male-5.png"
+            width={36}
+            height={36}
+            alt="Anna Adame"
+            className="rounded-full"
+          />
+          <div className="hidden text-sm md:block">
+            <div className="font-medium text-primary">Miguel Ramirez</div>
+            <div className="text-xs text-muted-foreground">Administrador</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ src/components/layout/inicio.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import Footer from "../footer";
+import AnimatedWaves from "../footer/animated-waves";
+import Navbar from "../navbar";
+
+import { usePathname } from 'next/navigation'
+
+function Inicio({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const pathname = usePathname()
+
+  if (pathname.startsWith('/admin')){
+    return <>{children}</>
+  }
+
+  return (
+    <>
+      <Navbar />
+      {children}
+      <AnimatedWaves />
+      <Footer />
+    </>
+  );
+}
+
+export default Inicio;
